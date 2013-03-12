a8c77dd9fc2fc371e2082818ed296199d4ec0569
lennguyen <len@123456@>
2013-03-12 21:25:39 +0700

    Modify template

--- src/CMS/Bundle/WebBundle/Controller/DefaultController.php
+++ src/CMS/Bundle/WebBundle/Controller/DefaultController.php
@@ -8,24 +8,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller {
 	public function indexAction($path) {
 		// echo $path;exit;
-		$menus = array (
-				'home' => 'Home',
-				'project' => 'Project',
-				'service' => 'Service',
-				'download' => 'Downloads',
-				'about' => 'About',
-				'contact' => 'Contact Us' 
-		);		
-		$active = null;
-		foreach ( $menus as $key => $value ) {
-			if ($path == $key) {
-				$active = $key;
-			}
-		}
+// 		$menus = array (
+// 				'home' => 'Home',
+// 				'project' => 'Project',
+// 				'service' => 'Service',
+// 				'download' => 'Downloads',
+// 				'about' => 'About',
+// 				'contact' => 'Contact Us' 
+// 		);		
+// 		$active = null;
+// 		foreach ( $menus as $key => $value ) {
+// 			if ($path == $key) {
+// 				$active = $key;
+// 			}
+// 		}
 		
 		return $this->render ( 'CMSWebBundle:Default:index.html.twig', array (
-				'menus' => $menus,
-				'active' => $active 
+				'path' => $path
 		) );
 	}
 }
--- src/CMS/Bundle/WebBundle/Controller/MenuController.php
+++ src/CMS/Bundle/WebBundle/Controller/MenuController.php
@@ -6,8 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MenuController extends Controller
 {
-    public function indexAction($slug)
+    public function indexAction($path)
     {
-        return $this->render('CMSWebBundle:Menu:index.html.twig', array('name' =>'Hai'));
+        $menus = array (
+                'home' => 'Home',
+                'project' => 'Project',
+                'service' => 'Service',
+                'download' => 'Downloads',
+                'about' => 'About',
+                'contact' => 'Contact Us'
+        );
+        $active = null;
+        foreach ( $menus as $key => $value ) {
+            if ($path == $key) {
+                $active = $key;
+            }
+        }
+        
+        return $this->render ( 'CMSWebBundle:Menu:index.html.twig', array (
+                'menus' => $menus,
+                'active' => $active
+        ) );
+        //return $this->render('CMSWebBundle:Menu:index.html.twig');
     }
 }
